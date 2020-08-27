709ca92f7fb3848db7f85b9dcf7af97a6d7bc0fb
viniciosbastos <aurelio.vinicios1996@gmail.com>
2020-08-27 18:24:30 -0300

    Apagando arquivos da configuração inicial

--- src/main/java/br/com/casamagalhaes/estagiocm/service/EstagiarioService.java
+++
@@ -1,9 +0,0 @@
-package br.com.casamagalhaes.estagiocm.service;
-
-import br.com.casamagalhaes.estagiocm.model.Estagiario;
-
-public interface EstagiarioService {
-
-    public String darBoasVindas(Estagiario estagiario);
-
-}
